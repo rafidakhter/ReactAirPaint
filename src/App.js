757bf869597006662d007b95f27b7e271148fcfc
Rafid <rafida9@gmail.com>
2020-11-19 21:53:47 -0500

    updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+import AppEngine from "./components/AppEngine";
+
+//importing components:
+import Inputs from "./components/inputs";
+import Screen from "./components/screen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Air Paint</h1>
+      <AppEngine />
     </div>
   );
 }
